df82af47f24eec0907ef87d3b0a7830ef7adde1e
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-07-20 18:57:22 +0400

    feat(product): create UnitType enum for measurement units

---
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/enums/UnitType.java
@@ -0,0 +1,12 @@
+package org.example.constructionmaterialsapi.enums;
+
+public enum UnitType {
+    KILOQRAM,
+    TON,
+    METR,
+    KVADRATMETR,
+    KUBMETR,
+    EDED,
+    KISE,
+    PACKA
+}

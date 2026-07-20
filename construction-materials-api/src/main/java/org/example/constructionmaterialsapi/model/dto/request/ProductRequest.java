a487b3c3167eb67bdc100cdea5a2bcf3c9e795b9
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-07-20 18:57:57 +0400

    feat(product): add ProductRequest and ProductResponse

--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/model/dto/request/ProductRequest.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/model/dto/request/ProductRequest.java
@@ -1,4 +1,40 @@
 package org.example.constructionmaterialsapi.model.dto.request;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.example.constructionmaterialsapi.enums.UnitType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductRequest {
+
+    @NotBlank(message = "Materialin adi bos ola bilmez")
+    @Size(min = 2, max = 100, message = "Ad 2-100 simvol araliginda olmalidir")
+    String name;
+
+    String description;
+
+    @NotNull(message = "Qiymet qeyd olunmalidir")
+    @Positive(message = "Qiymet musbet eded olmalidir")
+    BigDecimal price;
+    @NotNull(message = "Stok miqdari bos ola bilmez")
+    @Min(value = 0, message = "Stok menfi ola bilmez")
+    Integer stock;
+
+    @NotNull(message = "Olcu vahidi secilmelidir")
+    UnitType unit;
+
+    @NotNull(message = "Satici ID-si mutleqdir")
+    Long seller_id;
+
+    @NotNull(message = "Kateqoriya ID-si mutleqdir")
+    Long categoryId;
+
 }
--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/model/dto/response/ProductResponse.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/model/dto/response/ProductResponse.java
@@ -1,4 +1,30 @@
 package org.example.constructionmaterialsapi.model.dto.response;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.example.constructionmaterialsapi.enums.UnitType;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductResponse {
+
+    Long id;
+    String name;
+    String description;
+    BigDecimal price;
+    Integer stock;
+    UnitType unit;
+
+    //Satici
+    Long sellerId;
+    String sellerName;
+
+    //Kategoriya
+    Long categoryId;
+    String categoryName;
 }

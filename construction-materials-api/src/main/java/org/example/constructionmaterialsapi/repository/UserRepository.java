8e9bf19b2066981fbffe6fea0fdfcc2aee9fbe98
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-07-21 00:07:06 +0400

    feat: add UserRepository

--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/repository/UserRepository.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package org.example.constructionmaterialsapi.repository;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.example.constructionmaterialsapi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
 }

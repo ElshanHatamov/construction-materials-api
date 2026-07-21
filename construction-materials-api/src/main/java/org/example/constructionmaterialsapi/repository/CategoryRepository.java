c14058b8e8303e12508a77be0a60d9c340fbd35d
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-07-21 17:20:24 +0400

    feat: add repository query methods for user and category lookup

--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/repository/CategoryRepository.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import org.example.constructionmaterialsapi.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
 }
--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/repository/UserRepository.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/repository/UserRepository.java
@@ -5,7 +5,12 @@ import jakarta.validation.constraints.NotBlank;
 import org.example.constructionmaterialsapi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
+
+    Optional<User> findByEmail(String ownerEmail);
 }

e815e7d0c9e9aec50b324b93233817ab035b2fc7
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-08-06 11:02:52 +0400

    refactor(user): replace UserCreateRequest with RegisterRequest in UserService

--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/service/UserService.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/service/UserService.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.example.constructionmaterialsapi.exception.UserAlreadyExistsException;
 import org.example.constructionmaterialsapi.mapper.UserMapper;
-import org.example.constructionmaterialsapi.model.dto.request.UserCreateRequest;
+import org.example.constructionmaterialsapi.model.dto.request.RegisterRequest;
 import org.example.constructionmaterialsapi.model.dto.response.UserResponse;
 import org.example.constructionmaterialsapi.model.entity.User;
 import org.example.constructionmaterialsapi.repository.UserRepository;
@@ -19,7 +19,7 @@ public class UserService {
     UserRepository userRepository;
     UserMapper userMapper;
 
-    public UserResponse createUser(UserCreateRequest request) {
+    public UserResponse createUser(RegisterRequest request) {
         if (userRepository.existsByEmail((request.getEmail()))) {
             throw new UserAlreadyExistsException("Email already exists: " + request.getEmail());
         }

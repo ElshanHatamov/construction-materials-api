9a21791d1aae3fe9684fa080186f1b2b41cf3814
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-08-08 17:20:16 +0400

    feat(swagger): configure OpenAPI and Swagger documentation

--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/security/SwaggerConfig.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/security/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package org.example.constructionmaterialsapi.security;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +12,18 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Construction Materials API")
                         .version("1.0")
-                        .description("Tikinti materiallari idareetme sistemi ucun REST API Senedlesdirilmesi"));
+                        .description("Tikinti materiallari idareetme sistemi ucun REST API Senedlesdirilmesi"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }

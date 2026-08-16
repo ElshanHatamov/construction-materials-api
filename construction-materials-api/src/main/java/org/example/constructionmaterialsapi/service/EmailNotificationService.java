daef70f345e4cf3f3e60c1beb47644d73d37ae36
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-08-16 15:21:55 +0400

    feat(async): add @Async annotation to EmailNotificationService for asynchronous email delivery

--- construction-materials-api/src/main/java/org/example/constructionmaterialsapi/service/EmailNotificationService.java
+++ construction-materials-api/src/main/java/org/example/constructionmaterialsapi/service/EmailNotificationService.java
@@ -6,6 +6,7 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ public class EmailNotificationService {
 
     JavaMailSender javaMailSender;
 
+    @Async
     public void sendLoginSuccessEmail(String userEmail) {
         log.info("Asinxron email gonderme prosesi basladi. Thread: {}", Thread.currentThread().getName());
 
